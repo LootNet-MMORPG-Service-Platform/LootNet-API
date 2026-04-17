4a31e673f0a19b44f22af26a7c8483045dcbff49
DenysZachyniaiev <147748545+DenysZachyniaiev@users.noreply.github.com>
2026-04-17 18:57:06 +0200

    log model

---
+++ LootNet API/Models/Logs/AdminLog.cs
@@ -0,0 +1,13 @@
+﻿namespace LootNet_API.Models.Logs;
+
+public class AdminLog
+{
+    public Guid Id { get; set; }
+    public Guid AdminId { get; set; }
+
+    public string Action { get; set; } = string.Empty;
+    public string TargetUserId { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? Data { get; set; }
+}
